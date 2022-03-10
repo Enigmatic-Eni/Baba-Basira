ab23d35288c91811db04a4eca8cedc5af64a9eba
Enigmatic-Eni <92967097+Enigmatic-Eni@users.noreply.github.com>
2022-03-10 01:07:22 +0100

    Add files via upload

---
+++ Baba Basira/basira.js
@@ -0,0 +1,15 @@
+alert("An Essay About Baba Basira")
+
+console.log ("Baba Basira hails from Kwara State, He is a sucessful Entreprenuer.");
+
+console.log ("He has 4 wives and 70 children. He is also a womanizer.")
+
+console.log("He admires and aspires to be like Dangote.")
+
+console.log("He has 3 Estates and owns 6 cars. He has a potbelly but wishes to have 6 packs, so he goes to the gym quite often.")
+
+console.log("His strength and weakness are his flashlight and food")
+
+console.log("His favourite person is his flashlight")
+
+console.log("If he wasn't an Entreprenuer, he would have considered being a comedian.")
